7c0424741188a5bf98f6a1c5fb8269809f017b2c
EmililioFdez12 <emiliofdezglld12@gmail.com>
2024-03-04 11:07:50 +0100

    Terminar Ebay

--- SeleniumTarea4/src/test/java/SeleniumTarea4.java
+++ SeleniumTarea4/src/test/java/SeleniumTarea4.java
@@ -1,4 +1,9 @@
 import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -29,18 +34,33 @@ public class SeleniumTarea4 {
     driver.findElement(By.xpath("//*[@id=\"gh-ac\"]")).sendKeys("Play Station 5 \n");
     Thread.sleep(1000);
     // Vamos a la segunda pagina
-    driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul/li[62]/div[2]/span/span/nav/ol/li[2]/a")).click();
+    driver.findElement(By.linkText("2")).click();
     Thread.sleep(2000);
     // Tercera opcion
-    driver.findElement(By.xpath("//*[@id=\"item1d69a20d30\"]/div/div[2]/a/div/span")).click();
-    // Obtenemos el titulo de la consola que se vende
-//    WebElement elemento = driver.findElement(By.className("ux-textspans ux-textspans--BOLD"));
-//    System.out.println(elemento.getText()); 
+    WebElement ofertas = driver.findElement(By.xpath("//*[@id=\"srp-river-results\"]/ul"));
+    List<WebElement> listaOfertas  = ofertas.findElements(By.tagName("li"));
+        
+    if (listaOfertas.size() >= 3) {
+      WebElement terceraOferta = listaOfertas.get(2);
+      WebElement divOferta = terceraOferta.findElement(By.className("s-item__link"));
+         
+    } else {
+      System.out.println("hola");
+    }
     
-    // Vemos si el boton de enviar a la cesta esta
-    WebElement botonCesta = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/div/div[1]/div[2]/ul/li[2]/div/a"));   
-    assertTrue("Esta",botonCesta.isDisplayed());
     
+    
+
+    
+//    driver.findElement(By.xpath("//*[@id=\"item1d69a20d30\"]/div/div[2]/a/div/span")).click();
+//    // Obtenemos el titulo de la consola que se vende
+////    WebElement elemento = driver.findElement(By.className("ux-textspans ux-textspans--BOLD"));
+////    System.out.println(elemento.getText()); 
+//    
+//    // Vemos si el boton de enviar a la cesta esta
+//    WebElement botonCesta = driver.findElement(By.xpath("//*[@id=\"mainContent\"]/div[2]/div/div[1]/div[2]/ul/li[2]/div/a"));   
+//    assertTrue("Esta",botonCesta.isDisplayed());
+//    
     
   } catch (Exception e) {
     System.out.println(e.getMessage());
